ebfc3776bf759af013d11725820176a8c11deb98
ay0530 <edkdud53010@gmail.com>
2023-11-23 15:05:02 +0900

    ✨ feat: 로그인, 로그아웃 기능 구현(git pull 잘못해서 수동으로 다시 올림..

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const cors = require('cors'); // [이아영] 127.0.0.1 이슈 해결 패키지
 
-const loginRouter = require('./routes/users/login.routes.js');
+const authRouter = require('./routes/users/auth.routes.js');
 const signupRouter = require('./routes/users/signup.routes.js');
-const editRouter = require('./routes/posts/post.routes.js'); // [
-
+const editRouter = require('./routes/posts/post.routes.js');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.json());
@@ -14,11 +13,11 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/api', express.static(__dirname + '/views')); // [이아영] views/ 파일들 조회
+// app.use('/api', express.static(__dirname + '/views')); // [이아영] views/ 파일들 조회
 
-app.use("/api/users", loginRouter);
-app.use("/api/users", signupRouter);
-app.use("/api/posts", editRouter);
+app.use('/api/users', authRouter);
+app.use('/api/users', signupRouter);
+app.use('/api/posts', editRouter);
 
 // 서버 실행
 app.listen(app.get('port'), () => {
