44c17c0ee1ba38dddf204ff23a9395e4a6f2714a
nugabah <cxcx0917@gmail.com>
2023-11-26 02:36:09 +0900

    댓글 CRUD 가능한 api 구현. 유효성검사및프론트엔드 연결은 아직

--- routes/comments/comments.routes.js
+++ routes/comments/comments.routes.js
@@ -8,10 +8,10 @@ const prisma = new PrismaClient();
 // 댓글 생성
 router.post('/:post_id/comments', authMiddleware, async (req, res) => {
   try {
-    const { post_id } = req.params;
+    const post_id = parseInt(req.params.post_id);
     const { user_id } = res.locals.user[0];
     const { comment_content } = req.body;
-    const commentUser = await prisma.USER.findUnique({
+    const commentUser = await prisma.USER.findFirst({
       where: {
         user_id: +user_id,
       },
@@ -20,7 +20,7 @@ router.post('/:post_id/comments', authMiddleware, async (req, res) => {
       data: {
         post_id,
         user_id,
-        comment_name: commentUser.username,
+        comment_name : commentUser.username,
         comment_content,
       },
     });
@@ -34,18 +34,13 @@ router.post('/:post_id/comments', authMiddleware, async (req, res) => {
 // 댓글 조회
 router.get('/:post_id/comments', async (req, res) => {
   try {
-    const { post_id } = req.params;
+    const post_id = parseInt(req.params.post_id);
     const comments = await prisma.COMMENT.findMany({
       where: {
         post_id: +post_id,
       },
     });
-    if (!comments) {
-      throw new CustomError(
-        ErrorTypes.PostNotExistError,
-        '게시글이 존재하지 않습니다.',
-      );
-    }
+    await prisma.$disconnect(); // prisma 연결 끊기
     res.status(200).json({ data: comments });
   } catch (error) {
     res.status(400).json({ error });
@@ -58,14 +53,22 @@ router.put('/:post_id/comments', authMiddleware, async (req, res) => {
     const { post_id } = req.params;
     const { user_id } = res.locals.user[0];
     const { comment_id, comment_content } = req.body;
+    const numberComment_id = parseInt(comment_id);
+    const commentUser = await prisma.COMMENT.findFirst({
+      where: {
+        comment_id: +numberComment_id,
+        user_id: +user_id,
+      },
+    });
     const editComment = await prisma.COMMENT.update({
       where: {
-        comment_id: +comment_id,
+        comment_id: +numberComment_id,
       },
       data: {
         comment_content,
       },
     });
+    await prisma.$disconnect(); // prisma 연결 끊기
     return res.status(201).json({ data: editComment });
   } catch (error) {
     res.status(400).json({ error });
@@ -76,12 +79,21 @@ router.put('/:post_id/comments', authMiddleware, async (req, res) => {
 router.delete('/:post_id/comments', authMiddleware, async (req, res) => {
   try {
     const { post_id } = req.params;
+    const { user_id } = res.locals.user[0];
     const { comment_id } = req.body;
-    const deleteComment = await prisma.COMMENT.delete({
+    const numberComment_id = parseInt(comment_id);
+    const commentUser = await prisma.COMMENT.findFirst({
       where: {
-        comment_id: +comment_id,
+        comment_id: +numberComment_id,
+        user_id: +user_id,
       },
     });
+    const deleteComment = await prisma.COMMENT.delete({
+      where: {
+        comment_id: +numberComment_id,
+      },
+    });
+    await prisma.$disconnect(); // prisma 연결 끊기
     return res.status(201).json({ message: "삭제 완료" });
   } catch (error) {
     res.status(400).json({ error });
