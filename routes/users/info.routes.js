d60023582770763ff8a5f6d2a4fe450f6a299ae3
ay0530 <edkdud53010@gmail.com>
2023-11-27 03:27:14 +0900

    ✨update : 내정보조회 ejs 적용

--- routes/users/info.routes.js
+++ routes/users/info.routes.js
@@ -6,6 +6,31 @@ const prisma = new PrismaClient();
 const authMiddleware = require('../../middlewares/auth.middleware');
 
 // 내 정보 조회 API
+router.get('/detail', authMiddleware, async (req, res) => {
+  try {
+    const { user_id } = res.locals.user[0]; // user_id 조회
+    const user = await prisma.USER.findUnique({
+      select: {
+        username: true,
+        name: true,
+        email: true,
+        created_at: true,
+        updated_at: true,
+      },
+      where: { user_id },
+    });
+    await prisma.$disconnect(); // prisma 연결 끊기
+    if (!user) res.status(400).json({ errorMessage: '유저가 없습니다.' });
+
+    return res.render('index', {
+      data: user,
+      path: '/api/users/user-info/detail',
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 router.get('/', authMiddleware, async (req, res) => {
   try {
     const { user_id } = res.locals.user[0]; // user_id 조회
